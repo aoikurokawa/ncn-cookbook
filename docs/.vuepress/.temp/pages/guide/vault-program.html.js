05be3537d71dbd43fd64f5a287f1a4f21e8ba126
Aoi Kurokawa <62386689+aoikurokawa@users.noreply.github.com>
2024-12-06 06:07:49 +0900

    Docs: Operator (#7)
    * feat: operator
    
    * fix: update

--- docs/.vuepress/.temp/pages/guide/vault-program.html.js
+++ docs/.vuepress/.temp/pages/guide/vault-program.html.js
@@ -1,5 +1,5 @@
 import comp from "/mnt/nvme0n1/dev/ncn-cookbook/docs/.vuepress/.temp/pages/guide/vault-program.html.vue"
-const data = JSON.parse("{\"path\":\"/guide/vault-program.html\",\"title\":\"Vault Program\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":3,\"title\":\"Initialize Vault\",\"slug\":\"initialize-vault\",\"link\":\"#initialize-vault\",\"children\":[]}],\"git\":{\"updatedTime\":1733352952000,\"contributors\":[{\"name\":\"Aoi Kurokawa\",\"email\":\"62386689+aoikurokawa@users.noreply.github.com\",\"commits\":1,\"url\":\"https://github.com/Aoi Kurokawa\"},{\"name\":\"aoikurokawa\",\"email\":\"ht.so.ik0102@gmail.com\",\"commits\":1,\"url\":\"https://github.com/aoikurokawa\"}]},\"filePathRelative\":\"guide/vault-program.md\"}")
+const data = JSON.parse("{\"path\":\"/guide/vault-program.html\",\"title\":\"Vault Program\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":3,\"title\":\"Initialize Vault\",\"slug\":\"initialize-vault\",\"link\":\"#initialize-vault\",\"children\":[]}],\"git\":{\"updatedTime\":1733352952000,\"contributors\":[{\"name\":\"Aoi Kurokawa\",\"email\":\"62386689+aoikurokawa@users.noreply.github.com\",\"commits\":1,\"url\":\"https://github.com/Aoi Kurokawa\"}]},\"filePathRelative\":\"guide/vault-program.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
